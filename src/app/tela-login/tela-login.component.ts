1565b0c3c45e7e4cb8f9c0074eeeef36acbd511b
git <eldeen.works@gmail.com>
2023-06-25 13:42:50 -0300

    Adicionado tela de login

--- src/app/tela-login/tela-login.component.ts
+++ src/app/tela-login/tela-login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tela-login',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./tela-login.component.css']
 })
 export class TelaLoginComponent {
+  constructor(private router: Router){
 
+  }
+
+  RemoverTelaLogin() {
+    const windowLogin = document.getElementById('login');
+
+    windowLogin!.classList.remove('login-in');
+    windowLogin!.classList.add('login-out');
+    windowLogin!.style.animation = 'none';
+
+    setTimeout(() => windowLogin!.style.animation = '', 5); 
+    setTimeout(() => windowLogin!.style.display = 'none', 400); 
+    setTimeout(() => this.router.navigate(['/grafico-vendas']), 450);   
+    setTimeout(() => document.getElementById('navbar')!.style.visibility = 'visible', 200);
+  }
 }
+
+
